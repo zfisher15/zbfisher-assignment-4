dbf6c4dc79cb82f0b9cbac09547c375c8026ac22
zfisher15 <zibfisher15@gmail.com>
2025-09-25 15:43:35 -0400

    git add zbfisher_assignment_4.py
    git commit -m "Pass Test Case 1: Initial game setup with required variables"

---
+++ zbfisher_assignment_4.py
@@ -0,0 +1,12 @@
+student_name = "Zian"
+current_gpa = 4.0
+study_hours = 15
+social_points = 35
+stress_level = 30
+
+print(f"Welcome, {student_name}! Your current stats are:")
+print("GPA:", current_gpa)
+print("Study hours:", study_hours)
+print("Social points:", social_points)
+print("Stress level:", stress_level)
+print()
